6384962124b04da40a4f899eed85fcda15bb2685
Loskir <kl@sinker.ru>
2019-04-16 18:12:21 +0300

    :sparkles: Edit viewbox to -10:10 :bug: Fix some bugs :sparkles: Add some constants

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const SVG_SIZE_X = 20
+const SVG_SIZE_Y = 20
+const SVG_OFFSET_X = -10
+const SVG_OFFSET_Y = -10
+const ACCURACY = 0.1
+
 const svg = document.getElementById('svg')
 const functionPolyline = document.getElementById('function')
 const distancePolyline = document.getElementById('distance')
@@ -10,13 +16,12 @@ const circle = document.getElementById('circle')
 
 button.addEventListener('click', apply)
 
-let iterationRuns = 0
-let savedE
 let calculateAllowed = true
 let changes = false
 
 let formula
 let distanceFormulaText = ''
+let distanceFormula
 
 let x1 = 0
 let y1 = 0
@@ -28,6 +33,7 @@ function setFormula(text) {
 }
 function updateDistanceFormulaText() {
   distanceFormulaText = `sqrt(pow(x1-x, 2)+pow(y1-(${formula.toString()}) , 2))`
+  distanceFormula = math.parse(distanceFormulaText)
 }
 
 function getVal(x) {
@@ -35,16 +41,16 @@ function getVal(x) {
 }
 
 function convertY(y) {
-  return 500 - Math.max(-500, Math.min(y, 1000))
+  return SVG_SIZE_Y + 2*SVG_OFFSET_Y - Math.max(-500, Math.min(y, 500))
 }
 
 function drawFormula(polyline, formula, params = {}) {
-  let x = 0
+  let x = SVG_OFFSET_X
   let path = []
 
-  while (x <= 500) {
+  while (x <= SVG_SIZE_X+SVG_OFFSET_X) {
     path.push(`${x},${convertY(formula.eval({x, ...params}))}`)
-    x += 1
+    x += ACCURACY
   }
 
   polyline.setAttribute('points', path.join(' '))
@@ -64,42 +70,51 @@ function apply() {
   drawFormula(functionPolyline, formula)
 }
 
-svg.addEventListener('mousemove', handleMousemove)
-
-function handleMousemove(e) {
-  const gbcr = svg.getBoundingClientRect()
-
-  x1 = e.offsetX / gbcr.width * 500
-  y1 = 500 - e.offsetY / gbcr.height * 500
-  if (!calculateAllowed) {
-    changes = true
-    return
-  }
+function updateDistanceFormulaGraph() {
   const x = x1
 
   const distanceFormula = math.parse(distanceFormulaText)
   drawFormula(distanceGraphPolyline, distanceFormula, {x1, y1})
 
-  distancePolyline.setAttribute('points', `${x1},${500-y1} ${x},${getVal(x)}`)
+  distancePolyline.setAttribute('points', `${x1},${convertY(y1)} ${x},${getVal(x)}`)
   distance.innerHTML = distanceFormula.eval({x1, y1, x})
   circle.setAttribute('cx', x)
-  circle.setAttribute('cy', convertY(distanceFormula.eval({x1, y1, x}), 1000))
+  circle.setAttribute('cy', convertY(distanceFormula.eval({x1, y1, x})))
+}
+
+function updateDistancePolyline(x) {
+  distancePolyline.setAttribute('points', `${x1},${convertY(y1)} ${x},${getVal(x)}`)
+  distance.innerHTML = distanceFormula.eval({x1, y1, x})
+  circle.setAttribute('cx', x)
+  circle.setAttribute('cy', convertY(distanceFormula.eval({x1, y1, x})))
+}
+
+svg.addEventListener('mousemove', handleMousemove)
+
+function handleMousemove(e) {
+  const gbcr = svg.getBoundingClientRect()
+
+  x1 = e.offsetX / gbcr.width * SVG_SIZE_X + SVG_OFFSET_X
+  y1 = SVG_SIZE_Y - e.offsetY / gbcr.height * SVG_SIZE_Y + SVG_OFFSET_Y
+  if (!calculateAllowed) {
+    changes = true
+    return
+  }
+
+  updateDistanceFormulaGraph()
 
   calculate()
   calculateAllowed = false
   setTimeout(() => {
     calculateAllowed = true
   }, 50)
-}66
+}
 
 function calculate() {
   // console.log('calculate')
-  // changes = false
-
-  const distanceFormula = math.parse(distanceFormulaText)
 
   let x = x1
-  let lr = .1
+  let lr = .002
 
   const deriv = math.derivative(distanceFormulaText, 'x')
 
@@ -107,16 +122,17 @@ function calculate() {
 
   // iterationRuns++
 
-  let iterate = (currentRun) => {
+  let iterate = () => {
     // if (iterationRuns !== currentRun) {
     //   console.log(`exiting ${currentRun}`)
     //   return
     // }
     if (!iterateRunning) {
-      console.log(`exiting ${currentRun}`)
+      console.log(`exiting`)
       return
     }
     if (i === 100000) {
+      updateDistancePolyline(x)
       return
     }
 
@@ -124,6 +140,7 @@ function calculate() {
 
     if (Math.abs(delta) < 1e-14) {
       console.log(`exit on ${i}, delta: ${delta}`)
+      updateDistancePolyline(x)
       return
     }
 
@@ -138,13 +155,10 @@ function calculate() {
 
     if (i%200 === 199) {
       // console.log('update')
-      distancePolyline.setAttribute('points', `${x1},${500-y1} ${x},${getVal(x)}`)
-      distance.innerHTML = distanceFormula.eval({x1, y1, x})
-      circle.setAttribute('cx', x)
-      circle.setAttribute('cy', convertY(distanceFormula.eval({x1, y1, x}), 1000))
-      return setTimeout(() => iterate(currentRun), 10)
+      updateDistancePolyline(x)
+      return setTimeout(iterate, 10)
     }
-    return iterate(currentRun)
+    return iterate()
   }
 
   clearTimeout(iterateTimeout)
@@ -153,6 +167,8 @@ function calculate() {
     // let currentRun = iterationRuns
 
     console.log(`starting iterate`)
+
+    updateDistanceFormulaGraph()
 
     iterateRunning = true
     iterate()
